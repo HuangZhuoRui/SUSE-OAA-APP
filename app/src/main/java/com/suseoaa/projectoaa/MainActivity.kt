b571c1727dd38e0ca8106e79d1b5f9ebeb456770
HuangZhuoRui <huangzhuoruihzr@gmail.com>
2025-12-23 19:28:55 +0800

    修改代码架构

--- app/src/main/java/com/suseoaa/projectoaa/MainActivity.kt
+++ app/src/main/java/com/suseoaa/projectoaa/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import com.suseoaa.projectoaa.home.ui.main.MainScreen
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
@@ -14,9 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            // 计算屏幕大小
-            val windowSizeClass = calculateWindowSizeClass(this)
-            MainScreen(widthSizeClass = windowSizeClass.widthSizeClass)
+
         }
     }
 }
--- app/src/main/java/com/suseoaa/projectoaa/home/data/User.kt
+++
@@ -1,8 +0,0 @@
-package com.suseoaa.projectoaa.home.data
-
-// 简单的用户数据模型
-data class User(
-    val id: Int,
-    val name: String,
-    val bio: String
-)
