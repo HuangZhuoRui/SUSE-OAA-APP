7cbf2cee7bda39acab386686bb7425b4595b4000
HuangZhuoRui <huangzhuoruihzr@gmail.com>
2025-12-26 21:18:13 +0800

    修复注册页面的路由问题

--- app/src/main/java/com/suseoaa/projectoaa/feature/register/RegisterNavigation.kt
+++ app/src/main/java/com/suseoaa/projectoaa/feature/register/RegisterNavigation.kt
@@ -5,7 +5,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 
-const val REGISTER_ROUTE = "person_route"
+const val REGISTER_ROUTE = "register_route"
 
 fun NavController.navigateToRegister(navOptions: NavOptions? = null) {
     this.navigate(REGISTER_ROUTE, navOptions)
