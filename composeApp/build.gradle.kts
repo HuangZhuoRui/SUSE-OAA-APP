4d3fd97bf38d6694bf0837ba4c4a04deb60ec6d9
HuangZhuoRui <huangzhuoruihzr@gmail.com>
2026-01-30 00:56:38 +0800

    修复iOS端相册调用问题。

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -132,8 +132,8 @@ android {
         applicationId = "com.suseoaa.projectoaa"
         minSdk = 28
         targetSdk = 36
-        versionCode = 110111
-        versionName = "1.101.11"
+        versionCode = 110211
+        versionName = "1.102.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
