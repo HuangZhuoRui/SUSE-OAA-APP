30afc78861a5f8d5d829813f03a0c54702ea598d
HuangZhuoRui <huangzhuoruihzr@gmail.com>
2026-04-13 13:50:00 +0800

    新增锁定手机端竖屏状态

--- composeApp/src/androidMain/kotlin/com/suseoaa/projectoaa/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/suseoaa/projectoaa/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.suseoaa.projectoaa
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,6 +12,12 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 手机端锁定竖屏，平板端保持可旋转
+        if (resources.configuration.smallestScreenWidthDp < 600) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+
         enableEdgeToEdge()
 
         // 初始化验证码识别器 (加载 ddddocr 模型)
