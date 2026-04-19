df5d464649e3bdb9752a7242a601e073a35351cd
HuangZhuoRui <huangzhuoruihzr@gmail.com>
2026-04-19 19:40:35 +0800

    青蟹2.0震撼发布🎉

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,8 +13,8 @@ extensions.configure<ApplicationExtension>("android") {
         applicationId = "com.suseoaa.projectoaa"
         minSdk = 28
         targetSdk = 36
-        versionCode = 114354
-        versionName = "1.143.54"
+        versionCode = 200000
+        versionName = "2.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
